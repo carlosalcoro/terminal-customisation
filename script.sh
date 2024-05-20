65a5bb2cb3d6147f432bc8375a7cf2e527bb2aae
Carlos Cortés Rojas <carlosalcoro@Carloss-MacBook-Pro.local>
2024-05-20 12:13:17 +0200

    feat: add configurations / script files for local setup

---
+++ script.sh
@@ -0,0 +1,8 @@
+# starship
+brew install starship
+# lsdeluxe
+brew install lsd
+# iterm2 snazzy theme
+curl -Ls https://raw.githubusercontent.com/sindresorhus/iterm2-snazzy/main/Snazzy.itermcolors > /tmp/Snazzy.itermcolors && open /tmp/Snazzy.itermcolors
+# nano
+brew install nano
